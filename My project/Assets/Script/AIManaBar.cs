f23ed708a55c37b8f6ca259a98f675fe77149b8d
Emmanuel-Atiso <149158663+Emmanuel-Atiso@users.noreply.github.com>
2025-03-25 13:33:41 +0000

    emmanuel updated
    Allow AI card to play if there is enough mana

--- My project/Assets/Script/AIManaBar.cs
+++ My project/Assets/Script/AIManaBar.cs
@@ -1,20 +1,41 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class AIManaBar : MonoBehaviour
 {
     public Text AIManaText;
     public int currentMana = 3;
     public int maxMana = 10;
+    public int cardCost = 3;
 
-    public Image[] manaBlocks; // Array of 10 mana images
+    public Image[] manaBlocks; // Mana block images
     public Color fullColor = Color.cyan;
     public Color emptyColor = Color.gray;
 
     void Start()
     {
         UpdateManaBar();
+    }
+
+    void Update()
+    {
+        // Test spending mana
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UseMana(1);
+        }
+
+        // Test restoring mana
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestoreMana(1);
+        }
+
+        // Test AI playing card if enough mana
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TryPlayCard();
+        }
     }
 
     public void UseMana(int amount)
@@ -35,39 +56,24 @@ public class AIManaBar : MonoBehaviour
         {
             manaBlocks[i].color = i < currentMana ? fullColor : emptyColor;
         }
-    }
-    void Update()
-{
-    // Press Space to spend 1 mana
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-        UseMana(1);
-    }
 
-    // Press R to restore 1 mana
-    if (Input.GetKeyDown(KeyCode.R))
-    {
-        RestoreMana(1);
-    }
-    void UpdateManaBar()
-    {
-        // Update the blocks visually
-        for (int i = 0; i < manaBlocks.Length; i++)
-        {
-            manaBlocks[i].color = i < currentMana ? fullColor : emptyColor;
-        }
-
-        // Update the "X/10" text
         if (AIManaText != null)
         {
             AIManaText.text = currentMana + "/" + maxMana;
         }
     }
-    void Start()
-{
-    AIManaText.text = "It works!";
-    UpdateManaBar();
-}
-}
-}
 
+    public void TryPlayCard()
+    {
+        if (currentMana >= cardCost)
+        {
+            UseMana(cardCost);
+            Debug.Log("AI played a card!");
+            // You can add visual logic here later
+        }
+        else
+        {
+            Debug.Log("Not enough mana for AI to play.");
+        }
+    }
+}
